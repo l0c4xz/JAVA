5e75f5df847dbe015175ca16a78aac44fc02f4b9
lucca moreira <luccaferreira1a@gmail.com>
2025-03-31 14:27:50 -0300

    soma

---
+++ ex15.java
@@ -0,0 +1,9 @@
+public class ex15 {
+    public static void main(String[] args) {
+        int soma = 0;
+        for (int i = 1; i <= 15; i++) {
+            soma += i;
+        }
+        System.out.println("A soma dos números de 1 a 15 é: " + soma);
+    }
+}
