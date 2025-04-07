13f0a8c86afafabe02c2fe3b718ff2d80be99db7
lucca moreira <luccaferreira1a@gmail.com>
2025-04-07 11:07:00 -0300

    ex25

---
+++ ex25.java
@@ -0,0 +1,12 @@
+public class ex25
+{
+	public static void main(String[] args) {
+	    int numero = 1;
+	    
+	    while (numero <= 100){
+	        	System.out.println(numero);
+	         numero ++;
+	    }
+
+	}
+}
