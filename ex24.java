65f0e8463bc985c6d9504a421597825f833fbbee
lucca moreira <luccaferreira1a@gmail.com>
2025-04-07 11:00:17 -0300

    ex24

---
+++ ex24.java
@@ -0,0 +1,13 @@
+public class ex24
+{
+	public static void main(String[] args) {
+	    int soma = 0;
+	    int numero = 1;
+	    
+	    while (numero <= 10){
+	        soma += numero;
+	        numero ++;
+	    }
+		System.out.println("A soma dos numeros de 1 a 10 é: " + soma);
+	}
+}
