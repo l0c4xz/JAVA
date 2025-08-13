9aff8dbf7a9df9908a832bcb876d68d43ad355bc
lucca moreira <luccaferreira1a@gmail.com>
2025-08-13 10:47:04 -0300

    OOP

---
+++ zoo/src/Animal.java
@@ -0,0 +1,24 @@
+public class Animal {
+    String nome;
+    int idade;
+    String especie;
+    double peso;
+
+    public String getNome(){
+        return nome;
+    }public void setNome(String nome) {
+        this.nome = nome;
+    } public int getIdade(){
+        return idade;
+    }public void setIdade(int idade){
+        this.idade = idade;
+    }public String getEspecie() {
+        return especie;
+    }public void setEspecie(String especie){
+        this.especie = especie;
+    }public double getPeso(){
+        return peso;
+    }public void setPeso(double peso){
+        this.peso = peso;
+    }
+}
